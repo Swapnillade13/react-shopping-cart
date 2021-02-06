49a897d5ca0f1fdd7146b29eed90f34a5f3b9e34
Swapnil Lade <swapnil.careercode@gmail.com>
2021-02-06 20:12:51 +0530

    products compoments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
+import React from 'react'
+import Products from './components/products';
+import data from './data.json'
+
 // features 1
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">Shopping cart</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All right is reserved.
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(){
+    super();
+    this.state = {
+      products : data.products,
+      size : "",
+      sort : ""
+    }
+  }
+
+  render(){
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Shopping cart</a>
+        </header>
+        <main>
+          <div className="content">
+              <div className="main">
+                <Products products={this.state.products}></Products>
+              </div>
+              <div className="sidebar">
+                  side bar cart items
+              </div>
+          </div>
+        </main>
+        <footer>
+          All right is reserved.
+        </footer>
+      </div>
+    );
+}
 }
 
 export default App;
